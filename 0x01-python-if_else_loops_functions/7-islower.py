cec15fb213429741dc5072ce7ebfaee508b7e287
louzanizineddine <louzanizineddine@gmail.com>
2023-08-08 21:54:02 +0200

    task 6 7 9

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Returns True if c is lowercase cahracter
+# Returns False if c is not lowercase character 
+def islower(c):
+    if (ord(c) > 64) and (ord(c) < 91):
+        return (False)
+    if (ord(c) > 96) and (ord(c) < 123):
+        return (True)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_ld(i):
+    ld = 0
+    if i < 0:
+        temp = i * -1
+        ld = (temp % 10)
+    elif i > 0:
+        ld = (i % 10)
+    print(ld, end="")
+    return (ld)
