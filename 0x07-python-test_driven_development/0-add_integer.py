df3435002ab46cce5402603af94243d27ab2d79e
louzanizineddine <louzanizineddine@gmail.com>
2023-09-05 23:07:44 +0200

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is the "add_integer" module.
+
+The example module supplies one function, add_integer().  For example,
+
+>>> add_integer(12, 98)
+110
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers, either integers or floats, and returns their sum.
+
+    Args:
+        a (int, float): The first number to be added.
+        b (int, float, optional): The second number to be added. Defaults to 98
+
+    Returns:
+        int: The sum of a and b as an integer.
+
+    Raises:
+        TypeError: If either a or b is not an integer or a float.
+
+    Example:
+        >>> add_integer(2, 3)
+        5
+        >>> add_integer(2.5, 3)
+        5
+        >>> add_integer("2", 3)
+        Traceback (most recent call last):
+            ...
+        TypeError: a must be an integer or a float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    custed_a = int(a)
+    custed_b = int(b)
+    return custed_a + custed_b
