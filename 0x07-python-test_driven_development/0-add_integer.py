b3e7e8116a0a0d57440f4e29cda077b646ae88d1
louzanizineddine <louzanizineddine@gmail.com>
2023-09-07 10:50:32 +0200

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -35,10 +35,10 @@ def add_integer(a, b=98):
             ...
         TypeError: a must be an integer or a float
     """
-    
-    if not isinstance(a, (int, float)):
+
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     custed_a = int(a)
