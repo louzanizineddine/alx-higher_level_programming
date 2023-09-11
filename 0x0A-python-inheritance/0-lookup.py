fcb838dafe99d49ffc26cc9b01b49ee67ee0676e
louzanizineddine <louzanizineddine@gmail.com>
2023-09-12 01:18:07 +0200

    shoud nevr used from

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,7 +5,6 @@ Module: object_inspector
 Provides a function for inspecting Python object attributes and methods.
 
 Usage:
-    - Import the module: `import object_inspector`
     - Call `lookup(obj)` to get a list of attributes
     - and methods of the input object.
 """
