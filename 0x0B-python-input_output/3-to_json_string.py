c6aabadb7ed737eae8964df5fad1dddca929561a
louzanizineddine <louzanizineddine@gmail.com>
2023-09-12 11:38:07 +0200

    task 3 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """JSON TO STRING MODULE HAS ONE FUNCTION to_json_string"""
+
+import json
 
 
 def to_json_string(my_obj):
-    """stringify the oject into a string"""
+    """stringify the oject into a string no tests are needed"""
     return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """STRING TO JSON MODULE HAS ONE FUNCTION from_json_string"""
+
+import json
 
 
 def from_json_string(my_str):
-    """stringify the oject into a string"""
+    """stringify the oject into a string no test are needed"""
     return json.loads(my_str)
