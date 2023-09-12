2d151fc75f58b5a16fc5c77ab5b8add18b84c217
louzanizineddine <louzanizineddine@gmail.com>
2023-09-12 17:24:38 +0200

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""ADD ITEM MODULE HAS ONE FUNCTION """
+
+import sys
+import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+arguments = sys.argv[1:]
+all_arguments = []
+try:
+    all_args = load_from_json_file("add_item.json")
+except Exception as e:
+    save_to_json_file([], "add_item.json")
+all_arguments += (arguments)
+save_to_json_file(all_args, "add_item.json")
