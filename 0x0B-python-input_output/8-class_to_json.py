8055b0175f0fec8960a329e87e88ecab1066277b
louzanizineddine <louzanizineddine@gmail.com>
2023-09-13 14:36:08 +0200

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""MODULE CALSS TO JSON HAS ONLY ONE FUNCTION class_to_json"""
+
+
+def class_to_json(obj):
+    """using the builtin __dic__ for simple data structures"""
+    return obj.__dic__
