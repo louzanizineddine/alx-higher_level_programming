1e0b630df0656ed3e5967eb7b92dad48898b474a
louzanizineddine <louzanizineddine@gmail.com>
2023-12-01 21:11:38 +0100

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""send an email"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    res = requests.post(url, data={'email': email})
+    print(res.text)
