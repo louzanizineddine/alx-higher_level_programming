8fcb4ffa622807bba6c3d2b92fedf05c942a4796
louzanizineddine <louzanizineddine@gmail.com>
2023-12-01 21:18:17 +0100

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""error code"""
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
