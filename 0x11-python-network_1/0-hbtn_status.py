d00cdfa22c4ead421919afbbac54baed19ab196c
louzanizineddine <louzanizineddine@gmail.com>
2023-12-01 20:52:51 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         res = res.read()
-        print(f"Body response")
+        print(f"Body response:")
         print(f"\t- type: {type(res)}")
         print(f"\t- content: {res}")
         print(f"\t- utf8 content: {res.decode(encoding='utf8')}")
